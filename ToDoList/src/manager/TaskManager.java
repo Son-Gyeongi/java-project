e5c342cdd492ddf054f78d78f072045acb6f18eb
Son-Gyeongi <thsruddl77@gmail.com>
2025-02-15 18:21:15 +0900

    feat: add TaskManager Class

---
+++ ToDoList/src/manager/TaskManager.java
@@ -0,0 +1,42 @@
+package manager;
+
+import model.Task;
+
+import java.util.ArrayList;
+
+/*
+TaskManager - 할 일 관리 클래스
+할 일 목록을 관리하고 추가/삭제/수정/조회하는 기능 담당
+ */
+public class TaskManager {
+    private ArrayList<Task> tasks = new ArrayList<>();
+
+    // 할 일 추가
+    public void addTask(String description) {
+        // TODO 값이 비어있는 경우(호출하는 곳에서 검사해야하나?) 아니면 생성자에서?
+        tasks.add(new Task(description));
+    }
+
+    // 할 일 삭제
+    public void removeTask(int index) {
+        // 유효하지 않은 인덱스인 경우
+        if (!(0 <= index && index < tasks.size())) return;
+
+        tasks.remove(index);
+    }
+
+    // 할 일 수정
+    public void updateTask(int index, String description) {
+        // 유효하지 않은 인덱스인 경우
+        if (!(0 <= index && index < tasks.size())) return;
+
+        tasks.get(index).updateDescription(description);
+    }
+
+    // 할 일 목록 조회
+    public void displayTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + " : " + tasks.get(i));
+        }
+    }
+}
