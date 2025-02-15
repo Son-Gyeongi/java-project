47e499c7b52ba31f611f6aec7b585b5584afa7c7
Son-Gyeongi <thsruddl77@gmail.com>
2025-02-15 18:20:22 +0900

    feat: add Task Constructor

--- ToDoList/src/model/Task.java
+++ ToDoList/src/model/Task.java
@@ -1,17 +1,24 @@
 package model;
 
 /*
-Task - 할 일 객체
+Task - 할 일 클래스
 할 일 데이터를 관리하는 클래스
  */
 public class Task {
     private String description; // 할 일 데이터
 
+    public Task() {
+    }
+
+    public Task(String description) {
+        this.description = description;
+    }
+
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+    public void updateDescription(String description) {
         // TODO 여기서 입력값 description 유효성 검사할 게 있나?
         // 값이 비어있는 경우(호출하는 곳에서 검사해야하나?)
         this.description = description;
