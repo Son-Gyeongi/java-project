5c6d02e968eb92cc964388c47df75a50316dfa95
Son-Gyeongi <thsruddl77@gmail.com>
2025-02-16 23:03:09 +0900

    feat: add TaskFileManager loadFile()

--- ToDoList/src/util/TaskFileManager.java
+++ ToDoList/src/util/TaskFileManager.java
@@ -2,6 +2,7 @@ package util;
 
 import model.Task;
 
+import java.io.*;
 import java.util.ArrayList;
 
 /*
@@ -10,12 +11,41 @@ TaskFileManager - 파일 저장 및 불러오기 담당
  */
 public class TaskFileManager {
     private static final String filePath = System.getProperty("user.dir"); // 현재 실행 중인 프로그램 디렉토리
-    private static final String fileName = "src/textfile/tasks.txt";
+    private static final String fileName = "\\src\\textfile\\tasks.txt";
 
     // 저장하기
     public static void saveFile(ArrayList<Task> tasks) {
+        try {
+            BufferedWriter output = new BufferedWriter(new FileWriter(new File(filePath, fileName)));
 
+            for (int i = 0; i < tasks.size(); i++) {
+                output.write(tasks.get(i).toString());
+                output.newLine();
+            }
+
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-    // 불러오기
+
+    // 파일 불러오기
+    public static ArrayList<Task> loadFile() {
+        ArrayList<Task> list = new ArrayList<>();
+
+        try {
+            BufferedReader input = new BufferedReader(new FileReader(new File(filePath, fileName)));
+
+            String line = "";
+            for (int i = 0; (line = input.readLine()) != null; i++) {
+                list.add(new Task(line));
+            }
+
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
 }
